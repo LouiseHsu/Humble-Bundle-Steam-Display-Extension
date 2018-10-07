9704b54506a33e8d94a8220efa17d215bb613d32
LouiseHsu <36148254+LouiseHsu@users.noreply.github.com>
2018-10-07 12:23:38 -0700

    skeleton extension functional

--- background.js
+++ background.js
@@ -5,6 +5,15 @@
 'use strict';
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The colour is green.");
+        console.log("The colour is greenish.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'developer.chrome.com' },
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
     });
 });
