2cad21c88a1dd85a12a122d64d03b1b4e9c1dfee
LouiseHsu <36148254+LouiseHsu@users.noreply.github.com>
2018-11-29 04:38:43 -0800

    Update content.js

--- content.js
+++ content.js
@@ -2,10 +2,10 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.greeting == "getGameNames") {
-            var gameNames = document.getElementsByClassName('dd-captions');
+            var gameNames = document.getElementsByClassName('dd-image-box-text');
             for (var i = 0; i < gameNames.length; i++) {
-                var item = gameNames[i];
-                console.log(item.textContent);
+                var item = gameNames[i].textContent.trim;
+                console.log(item);
             }
             sendResponse({farewell: "goodbye"});
         }
